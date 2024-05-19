cc78570ab4bbfaf1325d7a1b0f0f84088d955c17
romanov-is-here <justbssprt@yandex.ru>
2024-05-19 23:38:34 +0300

    kinda positions

--- vue-app/src/components/StartPage.vue
+++ vue-app/src/components/StartPage.vue
@@ -2,8 +2,10 @@
 import {ref} from "vue";
 import {reactive} from "vue";
 import {VNetworkGraph} from "v-network-graph";
+import * as vNG from "v-network-graph"
 import "v-network-graph/lib/style.css"
 import axios from 'axios';
+import dagre from "dagre"
 
 export default {
   name: "StartPage",
@@ -16,15 +18,10 @@ export default {
     const isLoaderVisible = ref(false);
     const isGraphVisible = ref(false)
 
-    const graph = reactive({nodes : {}, edges:{}})
-    // const nodes = {
-    //   node1: { name: "Node 1" },
-    //   node2: { name: "Node 2" },
-    // }
-    //
-    // const edges = {
-    //   edge1: { source: "node1", target: "node2" },
-    // }
+    const data = reactive({nodes : {}, edges:{}})
+    const layouts = reactive({
+      nodes: {},
+    })
 
     const showGraph = () => {
       isInputsVisible.value = false
@@ -46,16 +43,84 @@ export default {
           }
         });
 
-        //this.myData = response.data;
-        console.log("received", response.data)
-        graph.nodes = response.data.nodes
-        graph.edges = response.data.edges
+        data.nodes = response.data.nodes
+        data.edges = response.data.edges
+        layout("TB")
         isGraphVisible.value = true
       } catch (error) {
         console.error('Ошибка при получении данных:', error);
       }
 
       isLoaderVisible.value = false
+    }
+
+    ///////////////////
+
+    const nodeSize = 80
+
+    const configs = vNG.defineConfigs({
+      view: {
+        autoPanAndZoomOnLoad: "fit-content",
+        onBeforeInitialDisplay: () => layout("TB"),
+      },
+      node: {
+        normal: { radius: nodeSize / 2 },
+        label: { direction: "center", color: "#fff" },
+      },
+      edge: {
+        normal: {
+          color: "#aaa",
+          width: 3,
+        },
+        margin: 4,
+        marker: {
+          target: {
+            type: "arrow",
+            width: 4,
+            height: 4,
+          },
+        },
+      },
+    })
+
+    function layout(direction) {
+      if (Object.keys(data.nodes).length <= 1 || Object.keys(data.edges).length == 0) {
+        return
+      }
+
+      // convert graph
+      // ref: https://github.com/dagrejs/dagre/wiki
+      const g = new dagre.graphlib.Graph()
+      // Set an object for the graph label
+      g.setGraph({
+        rankdir: direction,
+        nodesep: nodeSize * 2,
+        edgesep: nodeSize,
+        ranksep: nodeSize * 2,
+      })
+      // Default to assigning a new object as a label for each new edge.
+      g.setDefaultEdgeLabel(() => ({}))
+
+      // Add nodes to the graph. The first argument is the node id. The second is
+      // metadata about the node. In this case we're going to add labels to each of
+      // our nodes.
+      Object.entries(data.nodes).forEach(([nodeId, node]) => {
+        g.setNode(nodeId, { label: node.name, width: nodeSize, height: nodeSize })
+      })
+
+      // Add edges to the graph.
+      Object.values(data.edges).forEach(edge => {
+        g.setEdge(edge.source, edge.target)
+      })
+
+      dagre.layout(g)
+
+      g.nodes().forEach((nodeId) => {
+        // update node position
+        const x = g.node(nodeId).x
+        const y = g.node(nodeId).y
+        layouts.nodes[nodeId] = { x, y }
+      })
     }
 
     return {
@@ -65,7 +130,9 @@ export default {
       isGraphVisible,
       showGraph,
       reset,
-      graph
+      data,
+      layouts,
+      configs
     };
   }
 }
@@ -98,8 +165,10 @@ export default {
   <div v-if="isGraphVisible" class="full-page">
     <VNetworkGraph
         class="graph"
-        :nodes="graph.nodes"
-        :edges="graph.edges"
+        :nodes="data.nodes"
+        :edges="data.edges"
+        :layouts="layouts"
+        :configs="configs"
     />
   </div>
 </template>
