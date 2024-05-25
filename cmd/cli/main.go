c2a4da4b3149936ad93ae84da10c55a43cef4189
romanov-is-here <justbssprt@yandex.ru>
2024-05-25 07:56:00 +0300

    filter to cli calls

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/romanov-is-here/godeviz/internal/graph/depgraph"
 	"github.com/romanov-is-here/godeviz/internal/graph/lister"
 	"github.com/romanov-is-here/godeviz/internal/graph/painter"
 )
 
 func main() {
-	//err, g := lister.GetGraph("/Users/ilyaromanov/Projects/api")
-	err, g := lister.GetGraph("/Users/ilyaromanov/Projects/godeviz")
-	//err, g := lister.GetGraph("/Users/ilyaromanov/Projects/platform-go/cmd/devp")
+	//err, g := lister.GetGraph("/Users/ilyaromanov/Projects/api", depgraph.NewDefaultFilter())
+	err, g := lister.GetGraph("/Users/ilyaromanov/Projects/godeviz", depgraph.NewDefaultFilter())
+	//err, g := lister.GetGraph("/Users/ilyaromanov/Projects/platform-go/cmd/devp", depgraph.NewDefaultFilter())
 	//err := lister.GetGraph("./...")
 	if err != nil {
 		fmt.Println("error happened: ", err)
